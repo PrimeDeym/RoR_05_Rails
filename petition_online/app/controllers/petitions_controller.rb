43bd14f04fe9e02ab017dccf8872d8be9240d13e
PrimeDeym <primedeym@gmail.com>
2016-03-04 17:29:30 +0200

    update Petition

--- petition_online/app/controllers/petitions_controller.rb
+++ petition_online/app/controllers/petitions_controller.rb
@@ -4,16 +4,40 @@ class PetitionsController < ApplicationController
   end
   
   def new
+    @petition = Petition.new
   end
 
   def create
     @petition = Petition.new(petition_params)
-    @petition.save
-    redirect_to @petition
+    if @petition.save
+      redirect_to @petition
+    else
+      render 'new'
+    end
   end
 
   def show
     @petition = Petition.find(params[:id])
+  end
+
+  def edit
+    @petition = Petition.find(params[:id])
+  end
+
+  def update
+    @petition = Petition.find(params[:id])
+    if @petition.update(petition_params)
+      redirect_to @petition
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @petition = Petition.find(params[:id])
+    @petition.destroy
+
+    redirect_to petitions_path
   end
 
   private 
--- petition_online/app/models/petition.rb
+++ petition_online/app/models/petition.rb
@@ -1,4 +1,4 @@
 class Petition < ActiveRecord::Base
-  validates :title, presence: true,
-                    length: {minimum: 5}
+  validates :title, :text, presence: true,
+                    length: { minimum: 5 }
 end
