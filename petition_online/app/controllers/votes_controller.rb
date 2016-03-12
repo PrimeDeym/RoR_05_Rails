8b0391956ee0d68ad961172241f1c6a670420848
PrimeDeym <primedeym@gmail.com>
2016-03-12 15:33:11 +0200

    remove garbage

--- petition_online/app/controllers/votes_controller.rb
+++ petition_online/app/controllers/votes_controller.rb
@@ -1,14 +1,12 @@
 class VotesController < ApplicationController
   def create
     @petition = Petition.find(params[:petition_id])   
-    if @petition.user_id != current_user.id
-      @vote = @petition.votes.create(vote_params)
-      @vote[:user_id] = current_user.id
-      if @vote.save
-        flash[:notice] = 'Спасибо за ваш голос!'
-        redirect_to @petition
-      end  
-    end
+    @vote = @petition.votes.create(vote_params)
+    @vote[:user_id] = current_user.id
+    if @vote.save
+      flash[:notice] = 'Спасибо за ваш голос!'
+      redirect_to @petition
+    end  
   end
 
 private
